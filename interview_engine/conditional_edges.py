28fbb35028fe4ca6d5c1e6742fe961d53a619b51
sonjuyeong-00 <sonju9638@gmail.com>
2025-12-01 11:11:05 +0900

    수정

--- interview_engine/conditional_edges.py
+++ interview_engine/conditional_edges.py
@@ -1,4 +1,18 @@
 from interview_engine.state import InterviewState
 
 def route_loop(state:InterviewState) -> str:
-    pass
+    next_node = ""
+    etype = state.get("event_type")
+    
+    if state.get("await_human"):
+        return  "idle"  
+    
+    if etype == "init":
+        next_node = "problem_intro_agent"
+    elif etype == "strategy_submit":
+        next_node = "answer_classify_agent"
+        
+    
+    return next_node
+
+
--- interview_engine/state.py
+++ interview_engine/state.py
@@ -1,4 +1,20 @@
 from typing import TypedDict, Literal, List, Dict, Any, Optional
 
 class InterviewState(TypedDict):
+    # HITL 용
+    await_human = bool
+    event_type = Literal["init", 'strategy_submit']
+    
+    # 처음 input
+    username = str
+    
+    # 문제 
+    problem_description = str
+    
+    # 1번 agent
+    problem_intro_error = str
+    current_question_text =str
+    
+    # 2번 agent
+    
     pass

c5fd421efa59cb722f8b1711dd82d3fdf049c900
JungyuOO <rlawnsrb731@gmail.com>
2026-01-07 23:37:31 +0900

    feat: 배포 세팅용 백그라운드 파일

---
+++ backend/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+GUNICORN_WORKERS="${GUNICORN_WORKERS:-2}"
+GUNICORN_THREADS="${GUNICORN_THREADS:-4}"
+GUNICORN_TIMEOUT="${GUNICORN_TIMEOUT:-120}"
+
+exec gunicorn config.wsgi:application \
+  --bind 0.0.0.0:8000 \
+  --workers "$GUNICORN_WORKERS" \
+  --threads "$GUNICORN_THREADS" \
+  --timeout "$GUNICORN_TIMEOUT"
